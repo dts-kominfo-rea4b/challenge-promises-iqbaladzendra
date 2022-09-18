66fcddf39864b38bdf83f1a26202f3f71fa464b5
Iqbal Adzendra <iqbal.adzendra@gmail.com>
2022-09-18 17:17:53 +0700

    modif file promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const hasil = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()]);
+    const hasilFilter = hasil.flat().filter((item) => item.hasil === emosi);
+    return hasilFilter.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
